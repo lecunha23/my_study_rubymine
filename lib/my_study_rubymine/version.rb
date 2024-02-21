344587d0fa10debb9b46da70f520128d6e355ff8
Leandro Cunha <leandrojc23@gmail.com>
2024-02-20 22:44:15 -0300

    feat: add gem

---
+++ lib/my_study_rubymine/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module MyStudyRubymine
+  VERSION = "0.1.0"
+end
---
+++ spec/my_study_rubymine_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe MyStudyRubymine do
+  it "has a version number" do
+    expect(MyStudyRubymine::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
